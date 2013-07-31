99c26a972c2847f88eb2981556de5e102faa843c
Miike Ramos <mikeramos@gmail.com>
2013-07-31 01:10:32 -0700

    A test to check if the find route is working

--- spec/TestSpec.js
+++ spec/TestSpec.js
@@ -1,5 +1,21 @@
-describe('jasmine-node', function() {
+var mysql = require('mysql');
+var request = require('request');
+
+describe('Soundmap web server', function() {
   it('should pass', function() {
     expect(1+2).toEqual(3);
   });
+
+  it('should find the test user', function() {
+    var found = '';
+    request({
+      method: 'GET',
+      uri: 'http://localhost:3000/find/testuser/12345'
+    },
+    function(err, res, body) {
+      if (err) console.log(err);
+      found = body;
+      expect(found).toEqual(true);
+    });
+  })
 });
