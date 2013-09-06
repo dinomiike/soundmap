e928527c3152f1eb98c3d8bc8b3aba05370f4c33
Miike Ramos <mikeramos@gmail.com>
2013-09-05 22:31:13 -0700

    Testing environment variables

--- controllers/init.js
+++ controllers/init.js
@@ -4,13 +4,13 @@
 
 exports.initController = {
   root: function(req, res) {
-    console.log('inside the init controller');
     res.render('index', {
       title: 'soundmap'
     });
   },
 
   initialize: function(req, res) {
+    console.log('inside initialize:', process.env.SOUNDMAP_CLIENT_ID, process.env.SOUNDMAP_REDIRECT_URI);
     res.end(JSON.stringify({
       client_id: process.env.SOUNDMAP_CLIENT_ID,
       redirect_uri: process.env.SOUNDMAP_REDIRECT_URI
